f137526fed420c929e82d88c0915d789b736f2bb
dibbbbbbss <dibyanneupane@gmail.com>
2023-03-25 08:29:25 +0545

    day one

---
+++ control flow.dart
@@ -0,0 +1,31 @@
+
+void main(){
+String name = "dibyan neupane";
+
+print (name);
+
+print (12 + 13);
+
+var image = {
+	'tags': ['saturn'],
+	'url': '//path/to/saturn.jpg'
+
+} ; //var is used to declare variables...
+
+//control statements: 
+
+var year = 2004;
+
+if (year<2002){
+print('not borderline');
+}	else if (year>2002){
+print('borderline');
+}	else if(year>2004){
+print('no no no');
+}
+	else{
+print('error');
+}
+
+print(image);
+}
---
+++ variables.dart
@@ -0,0 +1,23 @@
+void main(){
+
+var name = 'Dibyan Neupane';
+var year = 1977 + 50;
+
+print (year + 50);
+print (name );
+
+
+if (year >= 2001){
+print('21st century schidzoid man');
+}else {
+print('not');
+}
+
+//this is a comment in dart?
+
+ 
+	
+}
+
+
+
